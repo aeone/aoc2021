f5c10f7d6865b0a2d517dd154a88de7e4c78cef0
Ryi <ex.arcana@gmail.com>
2021-12-03 10:57:17 +0000

    Day 3

--- src/aoc2021/core.clj
+++ src/aoc2021/core.clj
@@ -7,7 +7,15 @@
   [& args]
   (println "Hello, World!"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Util
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn parse-int [x] (Integer/parseInt x))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Part 1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn p1 [args]
   (->> args
@@ -39,8 +47,8 @@
        (partition 2 1)
        (map (fn [ts] (map #(apply + %) ts)))
        (filter #(apply < %))
-       (count)
-       ))
+       (count)))
+       
 
 (comment
   (p1b "199
@@ -55,6 +63,10 @@
 263")
 
   (p1b (slurp "src/aoc2021/p1-input.txt")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Part 2
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn p2 [args]
   (loop [hori 0 depth 0 steps (str/split-lines args)]
@@ -101,3 +113,76 @@ forward 2")
 
   (p2b (slurp "src/aoc2021/p2-input.txt")))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Part 3
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn p3 [args]
+  (let [rows (str/split-lines args)
+        transposed (apply mapv vector rows)
+        common (map frequencies transposed)
+        max (->> common (map #(apply max-key val %)) (map first))
+        min (->> common (map #(apply min-key val %)) (map first))
+        gamma (->> max (apply str) (#(Integer/parseInt % 2)))
+        epsilon (->> min (apply str) (#(Integer/parseInt % 2)))]
+    (* gamma epsilon)))
+
+(comment
+  (p3 "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010")
+
+  (p3 (slurp "src/aoc2021/p3-input.txt")))
+
+(defn min-key-or-zero [m] (if (and (> (count (vals m)) 1) (apply = (vals m))) \0 (first (apply min-key val m))))
+(defn p3b [args]
+  (let [rows (str/split-lines args)
+        oxy (loop [inputs rows
+                   position 0]
+              (let [transposed (apply mapv vector inputs)
+                    common (map frequencies transposed)
+                    max (->> common (map #(apply max-key val %)) (map first))
+                    max-pos (nth max position)
+                    filtered (filter #(= (nth % position) max-pos) inputs)]
+                (if (= 1 (count filtered))
+                  (first filtered)
+                  (recur filtered (+ position 1)))))
+        oxy (->> oxy (apply str) (#(Integer/parseInt % 2)))
+        cos (loop [inputs rows
+                   position 0]
+              (let [transposed (apply mapv vector inputs)
+                    common (map frequencies transposed)
+                    min (->> common (map min-key-or-zero))
+                    min-pos (nth min position)
+                    filtered (filter #(= (nth % position) min-pos) inputs)]
+                (if (>= 1 (count filtered))
+                  (first filtered)
+                  (recur filtered (+ position 1)))))
+        cos (->> cos (apply str) (#(Integer/parseInt % 2)))]
+    (* oxy cos)))
+
+(comment
+  (apply max-key val {0 5 1 5})
+  (p3b "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010")
+
+  (p3b (slurp "src/aoc2021/p3-input.txt")))
